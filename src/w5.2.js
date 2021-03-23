9ee664e03026fad0f002da8f494b56454391204d
Heccin <Ree>
2021-03-24 11:52:14 +1300

    switches

---
+++ src/w5.2.js
@@ -0,0 +1,64 @@
+// a switch is a block of code
+
+const person = 'john';
+switch (person) {
+    case 'jane':
+        console.log('hi jane');
+        break;
+        case '60':console.log('old');break;
+        case 'john':
+            console.log('hi john');
+            break;
+            case 'joe':
+                console.log('hi joe')
+                break;
+                case 'mary':
+                console.log('hi mary');
+                break;
+                default:
+                    console.log('hi')
+                    break;
+}
+
+const score = 2
+switch (score) {
+    case score >= 80:
+        console.log('A')
+        break;
+    case score >= 60:
+        console.log('B')
+        break;
+    case score >= 40:
+        console.log('C')
+        break;
+    case score >= 20:
+        console.log('D')
+        break;
+    default:
+        console.log('Fail')
+        break;
+}
+
+let gradStudent = 90 
+
+switch (false) {
+    case (gradStudent >= 90):
+        console.log('My grade is A+')
+        break
+        case (gradStudent <= 80):
+            console.log('My grade is B+')
+            break           
+}
+
+let Tony = {
+    name : 'Tony',
+    age : 20,
+    driver: null
+};
+
+if (Tony.age >= 16) {
+    Tony.driver = 'yes';
+} else {
+    Tony.driver = 'no';
+}
+console.log(Tony)
--- src/w5.js
+++ src/w5.js
@@ -28,3 +28,49 @@ for (let i =0;; i++) {
 //loop counters are 0-index based, skip the middle statement and break out of the loop using the condition (i > 1)
 //always have a condition to avoid infinite loops 
 
+//multiple statements can be seperated by commas.
+
+let counter = 0;
+function inc() { counter++; } for (let i =0; i < 10; i++, inc());
+console.log(counter); // 10
+
+//inc() function is used to increment the value of a global variable "counter"
+
+//loops can increment or decrement 
+
+//bracketless for-loop does not work well let. this code generates an error 
+//for ( var i = 0; i < 10; i++) let x = i;
+
+//all variables defined with let requires their own local scope
+for ( var i = 0; i < 10; i++) { 
+    let x = i; 
+    console.log(x)} 
+//const also works in this instance 
+
+//nested loops can be used as the iterable statement od another for loop. 
+for (let y = 0; y < 2; y++)
+for (let x = 0; x < 2; x++)
+console.log(x, y);
+ //the output is all combinations between x and y 
+
+ // continue allows you to skip an iteration step 
+ for (let i = 0 ; i < 3; i++) {if (i == 1) continue ; 
+    console.log(i); }
+// the output skips 1 
+
+//loop can be broken by using the keyword break. conditions can be applied 
+
+for (let i = 0;; i++) {
+    console.log(" loop" ); break; 
+} ;
+
+//use array.length to check arrays
+//there is no index, no stop or break has been defined
+let array = [0, 1, 2,]
+for (let value of array)
+console.log(value);
+
+
+
+
+
