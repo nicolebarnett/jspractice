711fa9545cf65218f43e973f0e0e1104abb3cafb
Heccin <Ree>
2021-04-14 10:58:42 +1200

    reee

--- src/w8.js
+++ src/w8.js
@@ -1,19 +1,19 @@
-// ["215", true, false, 2, 2.1, [], null] -> [string]
+// // ["215", true, false, 2, 2.1, [], null] -> [string]
 
-function arrayDataTypes(arr) {
-    let newArr = [];
-    for(let i = 0; i < arr.length; i++) {
-        newArr.push(typeof arr[i]) 
-    }
-    return newArr
-}
-console.log(arrayDataTypes(["215", true, false, 2, 2.1, [], null]))
+// function arrayDataTypes(arr) {
+//     let newArr = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         newArr.push(typeof arr[i]) 
+//     }
+//     return newArr
+// }
+// console.log(arrayDataTypes(["215", true, false, 2, 2.1, [], null]))
 
-//now we make 2 lines!
+// //now we make 2 lines!
 
-function arrayDataTypes(arr) {
-    return arr.map(x => typeof x)
-}
+// function arrayDataTypes(arr) {
+//     return arr.map(x => typeof x)
+// }
 
 const arrayDataTypes = arr => arr.map(x => typeof x)
-console.log(arrayDataTypes)([["215", true, false, 2, 2.1, [], null])
+console.log(arrayDataTypes(["215", true, false, 2, 2.1, [], null]))
