b7e093f70e2eac58c364d51e9784d733240b4d31
Heccin <Ree>
2021-04-14 10:18:35 +1200

    fun arrays

--- src/w8.js
+++ src/w8.js
@@ -1,5 +1,10 @@
 // ["215", true, false, 2, 2.1, [], null] -> [string]
 
-function arrayDataTypes () {
-    
+function arrayDataTypes(arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(typeof arr[i]) 
+    }
+    return newArr
 }
+console.log(arrayDataTypes(["215", true, false, 2, 2.1, [], null]))
