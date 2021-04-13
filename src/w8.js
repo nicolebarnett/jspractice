2b1d5360089deb6c104070ef7c0e0c01790d8590
Heccin <Ree>
2021-04-14 10:31:34 +1200

    make it smol!

--- src/w8.js
+++ src/w8.js
@@ -8,3 +8,12 @@ function arrayDataTypes(arr) {
     return newArr
 }
 console.log(arrayDataTypes(["215", true, false, 2, 2.1, [], null]))
+
+//now we make 2 lines!
+
+function arrayDataTypes(arr) {
+    return arr.map(x => typeof x)
+}
+
+const arrayDataTypes = arr => arr.map(x => typeof x)
+console.log(arrayDataTypes)([["215", true, false, 2, 2.1, [], null])
